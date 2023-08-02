d207d9b8c5993b8436515622688e86bc27eb7e92
Ashmit Yadav <beingashmit@gmail.com>
2023-08-02 09:26:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { Box,Heading } from '@chakra-ui/react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Box textAlign="center" padding={4}>
+        <Heading as="h1" size="xl">
+          Red and white
+        </Heading>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
