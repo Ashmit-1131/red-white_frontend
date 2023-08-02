901cddc6fc09052996a00f781d4bb38935f191dc
Ashmit Yadav <beingashmit@gmail.com>
2023-08-02 10:19:27 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
-import { Box,Heading } from '@chakra-ui/react';
+import { Box,Heading, Spacer } from '@chakra-ui/react';
+import Navbar from './Component/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Box textAlign="center" padding={4}>
+     
+
+      <Navbar/>
+      <Box textAlign="center" background="gray" height="600" padding={4}>
         <Heading as="h1" size="xl">
-          Red and white
+         Welcome to the <span style={{ color: 'red' }}>Red</span> and <span style={{ color: 'white' }}>white</span>
+         <Spacer/> 
+          Multimedia blogging app
         </Heading>
       </Box>
     </div>
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { ChakraProvider, CSSReset, Box, Flex, Spacer, Link, Button, IconButton, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons';
+
+function Navbar() {
+  return (
+   
+      <Box borderBottom="1px" borderColor="gray.200">
+        <Flex justifyContent="space-between" padding={4}>
+          <Box>
+            <Link fontSize="xl" fontWeight="bold" color="teal.500" href="/">
+              My Blog App
+            </Link>
+          </Box>
+          <Box borderBottom="1px" borderColor="gray.200">
+        <Flex justifyContent="space-between" padding={12}>
+          <Box>
+            <Link fontSize="xl" fontWeight="bold" color="blue.500" href="/blogs" marginRight="4">
+              Blogs
+            </Link>
+          </Box>
+          <Link fontSize="xl" fontWeight="bold" color="red.500" href="/login" marginRight="4">
+            Login
+          </Link>
+          <Link fontSize="xl" fontWeight="bold" color="red.500" href="/signup" marginRight="4">
+            Sign Up
+          </Link>
+          <Link fontSize="xl" fontWeight="bold" color="yellow.500" href="/profile">
+            Profile
+          </Link>
+        </Flex>
+      </Box>
+          <Box display={{ base: 'block', md: 'none' }}>
+            <Menu>
+              <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" />
+              <MenuList>
+                <MenuItem>
+                  <Link href="/blogs">Blogs</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href="/login">Login</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href="/signup">Sign Up</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link href="/profile">Profile</Link>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Flex>
+      </Box>
+    
+  );
+}
+
+export default Navbar;
